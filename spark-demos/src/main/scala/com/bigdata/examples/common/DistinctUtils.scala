258761a78ab2e0376283fa9672188e6e375c54aa
zhzhh <zhihongzhang_hi@163.com>
2017-05-24 21:13:14 +0800

    distinct by groupby utils

---
+++ spark-demos/src/main/scala/com/bigdata/examples/common/DistinctUtils.scala
@@ -0,0 +1,26 @@
+package com.bigdata.examples.common
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by Administrator on 2017/5/24.
+  */
+object DistinctUtils {
+  def main(args : Array[String]) :Unit = {
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN);
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.ERROR);
+
+    val conf = new SparkConf().setAppName("KafkaDataTest2").setMaster("local[3]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+
+    val rawRdd = sc.textFile(Array("E:\\Workspace\\Scala\\spark-demos\\src\\main\\data\\text\\file1","E:\\Workspace\\Scala\\spark-demos\\src\\main\\data\\text\\file2").mkString(","))
+    val groupRdd = rawRdd.filter(_.trim().length>0).map(line=>(line.trim,"")).groupByKey()sortByKey()
+    val sortRdd = groupRdd.map(x => {
+      x._1
+    })
+    sortRdd.foreach(println)
+  }
+}
